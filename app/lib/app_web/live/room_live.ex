860d568c669117810d229459136eff3399505e11
Alfie Phillips <17173@st-ives.cornwall.sch.uk>
2021-08-26 00:06:23 +0100

    handle_info

--- app/lib/app_web/live/room_live.ex
+++ app/lib/app_web/live/room_live.ex
@@ -7,12 +7,19 @@ defmodule AppWeb.RoomLive do
     topic = "room:" <> room_id
     AppWeb.Endpoint.subscribe(topic)
 
-    {:ok, assign(socket, room_id: room_id, topic: topic, messages: ["Hello", "How are things?"])}
+    {:ok, assign(socket, room_id: room_id, topic: topic, messages: [])}
   end
 
   @impl true
   def handle_event("submit_message", %{"chat" => %{"message" => message}}, socket) do
     Logger.info(message: message)
+    AppWeb.Endpoint.broadcast(socket.assigns.topic, "new-message", message)
     {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info(%{event: "new-message", payload: message}, socket) do
+    Logger.info(payload: message)
+    {:noreply, assign(socket, messages: socket.assigns.messages ++ [message])}
   end
 end
