151cea2ceeab2406b0244ffa02c233f231d7f4ec
Alfie Phillips <17173@st-ives.cornwall.sch.uk>
2021-08-25 23:53:07 +0100

    handle submit message

--- app/lib/app_web/live/room_live.ex
+++ app/lib/app_web/live/room_live.ex
@@ -4,6 +4,15 @@ defmodule AppWeb.RoomLive do
 
   @impl true
   def mount(%{"id" => room_id}, _session, socket) do
-    {:ok, assign(socket, room_id: room_id)}
+    topic = "room:" <> room_id
+    AppWeb.Endpoint.subscribe(topic)
+
+    {:ok, assign(socket, room_id: room_id, topic: topic, messages: ["Hello", "How are things?"])}
+  end
+
+  @impl true
+  def handle_event("submit_message", %{"chat" => %{"message" => message}}, socket) do
+    Logger.info(message: message)
+    {:noreply, socket}
   end
 end
